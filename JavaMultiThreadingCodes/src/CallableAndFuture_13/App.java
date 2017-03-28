eab45236f74ef406b9f0480937c644f359303a30
IrisQ <eloise_zyf@hotmail.com>
2017-03-28 10:16:43 -0400

    Update App.java
    Add notes

--- JavaMultiThreadingCodes/src/CallableAndFuture_13/App.java
+++ JavaMultiThreadingCodes/src/CallableAndFuture_13/App.java
@@ -10,41 +10,30 @@ import java.util.concurrent.*;
  * your threads to throw exceptions. Plus, Future allows you to control your
  * threads, checking to see if they’re running or not, waiting for results and
  * even interrupting them or de-scheduling them.
- * <p>
+
  * {@link java.lang.Runnable}
  * is the default abstraction for creating a task in Java. It has a single
  * method {@link Runnable#run()}
  * that accepts no arguments and returns no value, nor it can throw
  * any checked exception. To overcome these limitations, Java 5 introduced a new
  * task abstraction through {@link java.util.concurrent.Callable} interface.
- * <br><br>
- * Codes with minor comments are from
- * <a href="http://www.caveofprogramming.com/youtube/">
- * <em>http://www.caveofprogramming.com/youtube/</em>
- * </a>
- * <br>
- * also freely available at
- * <a href="https://www.udemy.com/java-multithreading/?couponCode=FREE">
- * <em>https://www.udemy.com/java-multithreading/?couponCode=FREE</em>
- * </a>
- *
- * @author Z.B. Celik <celik.berkay@gmail.com>
  */
 public class App {
 
     public static void main(String[] args) throws InterruptedException {
         ExecutorService executor = Executors.newCachedThreadPool();
 
-        //anonymous call of Callable
-        Future<Integer> future = executor.submit(new Callable<Integer>() {
-
+        // Callable instead of Runnable, can return result instead of void
+        // if don't want to use return value now, but may use it in the future:
+        // Future<?> future = executor.submit(new Callable<Void>() {  
+        Future<Integer> future = executor.submit(new Callable<Integer>() {  
+        
             @Override
-            //return value is Integer
-            public Integer call() throws TimeoutException {
+            public Integer call() throws TimeoutException {     
                 Random random = new Random();
                 int duration = random.nextInt(4000);
                 if (duration > 2000) {
-                    throw new TimeoutException ("Sleeping for too long.");
+                    throw new TimeoutException ("Sleeping for too long.");  
                 }
 
                 System.out.println("Starting ...");
@@ -57,15 +46,14 @@ public class App {
         });
 
         executor.shutdown();
-//        executor.awaitTermination(1, TimeUnit.DAYS);
-        try {
-            //get returned value from call()
-            System.out.println("Result is: " + future.get());
-
+        // can wait for threads to finish, if not, when use future.get() it will block till threads finish
+        // executor.awaitTermination(1, TimeUnit.DAYS);    
+        try {            
+            System.out.println("Result is: " + future.get());   //get returned value from call()
         } catch (InterruptedException ignored) {
         } catch (ExecutionException e) {
             TimeoutException ex = (TimeoutException) e.getCause();
-            System.out.println(ex.getMessage());
+            System.out.println(ex.getMessage());                // Will print "sleeping for too long"
         }
     }
 
