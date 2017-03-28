a7381fe8476f0cb8fd712001334621d9abaa597d
IrisQ <eloise_zyf@hotmail.com>
2017-03-28 10:33:58 -0400

    Update App.java

--- JavaMultiThreadingCodes/src/InterruptingThreads14/App.java
+++ JavaMultiThreadingCodes/src/InterruptingThreads14/App.java
@@ -3,66 +3,50 @@ package InterruptingThreads14;
 import java.util.concurrent.*;
 
 /**
- * <b>How to interrupt running threads in Java using the built-in thread
- * interruption mechanism.</b>
- * <br><br>
+ * <b>How to interrupt running threads in Java using the built-in thread interruption mechanism.</b>
  * Source:
  * <a href="http://www.javamex.com/tutorials/threads/thread_interruption.shtml">
  * http://www.javamex.com/tutorials/threads/thread_interruption.shtml</a>
- * <p>
- * Incidentally, it is important NOT to confuse thread interruption with either
- * software interrupts (where the CPU automatically interrupts the current
- * instruction flow in order to call a registered piece of code periodically— as
- * in fact happens to drive the thread scheduler) and hardware interrupts (where
- * the CPU automatically performs a similar task in response to some hardware
- * signal).
- * <br><br>
- * Codes with minor comments are from
- * <a href="http://www.caveofprogramming.com/youtube/">
- * <em>http://www.caveofprogramming.com/youtube/</em>
- * </a>
- * <br>
- * also freely available at
- * <a href="https://www.udemy.com/java-multithreading/?couponCode=FREE">
- * <em>https://www.udemy.com/java-multithreading/?couponCode=FREE</em>
- * </a>
- *
- * @author Z.B. Celik <celik.berkay@gmail.com>
+ 
+ * Thread vs software vs hardware interruption:
+ * Software: CPU automatically interrupts the current instruction flow in order to call a registered piece of code periodically
+ * as in fact happens to drive the thread scheduler
+ * Hardware: CPU automatically performs a similar task in response to some hardware signal.
  */
 public class App {
 
     public static void main(String[] args) throws InterruptedException {
 
         System.out.println("Starting.");
-
         ExecutorService executor = Executors.newCachedThreadPool();
-
+ 
         Future<?> fu = executor.submit(new Callable<Void>() {
-
             @Override
             public Void call() throws Exception {
-
                 for (int i = 0; i < 1E8; i++) {
                     if (Thread.currentThread().isInterrupted()) {
                         System.out.printf("Interrupted at %d !!!", i);
                         break;
                     }
-                }
-
+                    // instead of using Thread.currentThread().isInterrupted() to detect interrupt, 
+                    // can also use sleep, which may throw InterruptException:
+                    /*try {
+                        Thread.sleep(1);
+                    } catch (InterruptException e) {
+                        System.out.println("Interrupted!");
+                        break;
+                    }*/                    
+                }                
                 return null;
             }
         });
 
         executor.shutdown();
         Thread.sleep(500);
-
-        /*
-        in this example, there are different ways you can interrupt a thread
-        execution.
-         */
+        // t1.interrupt() won't stop thread, will only set flag that thread is interrupted
 
         //JavaDoc: http://docs.oracle.com/javase/8/docs/api/java/util/concurrent/Future.html#cancel-boolean-
-//        fu.cancel(true);
+        //fu.cancel(true);  
 
         //JavaDoc: http://docs.oracle.com/javase/8/docs/api/java/util/concurrent/ExecutorService.html#shutdownNow--
         executor.shutdownNow();
