cbb5ef9fddde709232451622fc48eca4bb8d8a53
IrisQ <eloise_zyf@hotmail.com>
2017-03-27 22:39:44 -0400

    Update Processor.java

--- JavaMultiThreadingCodes/src/WaitAndNotify_8/Processor.java
+++ JavaMultiThreadingCodes/src/WaitAndNotify_8/Processor.java
@@ -3,37 +3,20 @@ package WaitAndNotify_8;
 import java.util.Scanner;
 
 /**
- * Some background knowledge<br>
- * Source: <em>http://www.programcreek.com/2009/02/notify-and-wait-example/</em>
- * <br><br>
- * {@code synchronized} keyword is used for exclusive accessing. To make a
- * method {@code synchronized}, simply add the {@code synchronized} keyword to its
- * declaration.<be>
- * Then no two invocations of synchronized methods on the same object can
+ * Some background knowledge: <em>http://www.programcreek.com/2009/02/notify-and-wait-example/</em>
+ * synchronized keyword is used for exclusive accessing. 
+ * no two invocations of synchronized methods on the same object can
  * interleave with each other.
- * <br>
  * Synchronized statements must specify the object that
  * provides the intrinsic lock. When {@code synchronized(this)} is used, you
  * have to avoid to synchronizing invocations of other objects' methods.
- * <br>
+
  * {@link Object#wait()} tells
  * the calling thread to give up the lock and go to sleep (not polling) until
  * some other thread enters the same lock and calls {@link Object#notify()}.
- * <br>
+
  * {@link Object#notify()} wakes up the first thread that called wait() on
  * the same object.
- * <br><br>
- * Codes with minor comments are from
- * <a href="http://www.caveofprogramming.com/youtube/">
- * <em>http://www.caveofprogramming.com/youtube/</em>
- * </a>
- * <br>
- * also freely available at
- * <a href="https://www.udemy.com/java-multithreading/?couponCode=FREE">
- *     <em>https://www.udemy.com/java-multithreading/?couponCode=FREE</em>
- * </a>
- *
- * @author Z.B. Celik <celik.berkay@gmail.com>
  */
 public class Processor {
 
@@ -48,20 +31,20 @@ public class Processor {
     public void produce() throws InterruptedException {
         synchronized (this) {
             System.out.println("Producer thread running ....");
-            wait();//this.wait() is fine.
+            wait();                             // more resource efficient than simply use while loop.
             System.out.println("Resumed.");
         }
     }
 
     public void consume() throws InterruptedException {
         Scanner scanner = new Scanner(System.in);
-        Thread.sleep(2000);
+        Thread.sleep(2000);         // make sure produce() before comsume()
         synchronized (this) {
             System.out.println("Waiting for return key.");
-            scanner.nextLine();
+            scanner.nextLine();     // wait till press return key on keyboard
             System.out.println("Return key pressed.");
-            notify();
-            Thread.sleep(5000);
+            notify();               // can only be called in synchronized, call all other threads, if they are waiting, they can wake up
+            Thread.sleep(5000);     // this will finish first, before another thread can access
             System.out.println("Consumption done.");
         }
     }
